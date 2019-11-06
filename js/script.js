342290a1e87231be311dbb751bd5cb2da1013d97
anouar el ghazaoui <anouar.eg@live.nl>
2019-11-06 11:16:29 +0100

    US-12

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function changeImg() {
     var image = document.getElementById('myImage');
     if (image.src.match("images/ik.jpg")) {
-        image.src = "images/ik2.jpg";
+        image.src = "images/ik3.jpg";
     } else {
         image.src = "images/ik.jpg";
     }
@@ -9,7 +9,6 @@ function changeImg() {
 
 var likes=0, dislikes=0;
 
-//Functions to increase likes and immediately calculate bar widths
 function like(){
 	likes++;
 	calculateBar();
@@ -19,26 +18,22 @@ function dislike(){
 	calculateBar();
 }
 
-//Calculates bar widths
 function calculateBar(){
 	var total= likes+dislikes;
-    //Simple math to calculate percentages
 	var percentageLikes=(likes/total)*100;
 	var percentageDislikes=(dislikes/total)*100;
 
-    //We need to apply the widths to our elements
 	document.getElementById('likes').style.width=percentageLikes.toString()+"%";
 	document.getElementById('dislikes').style.width=percentageDislikes.toString()+"%";
     
-    //We add the numbers on the buttons, just to show how to
     document.getElementById('likeButton').value="Like ("+likes.toString()+")";
     document.getElementById('dislikeButton').value="Disike ("+dislikes.toString()+")";
 
 }
 
-calculateBar();
 
 
 datum= new Date()
     document.getElementById("vandaag").innerHTML = datum.getDate() + "-" + (datum.getMonth()+1)
     + "-" + datum.getFullYear();
+
