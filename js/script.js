f54e6f1ccdca81fbf9f88249d3213e3fda42a706
anouar el ghazaoui <anouar.eg@live.nl>
2019-11-06 08:29:04 +0100

    US-06

--- js/script.js
+++ js/script.js
@@ -2,8 +2,17 @@ function changeImg() {
     var image = document.getElementById('myImage');
     if (image.src.match("images/ik.jpg")) {
         image.src = "images/ik2.jpg";
-    }
-    else {
+    } else {
         image.src = "images/ik.jpg";
     }
 }
+
+var clicks = 14;
+
+document.getElementById("clicks").innerHTML = clicks;
+
+$('.like-counter').click(function () {
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+    $('.like-counter').addClass("liked");
+});
